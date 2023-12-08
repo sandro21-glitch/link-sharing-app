aefd6f32704b7a7b28d25a3c03872a3d63ef41c9
Sandro <vipsandro11@gmail.com>
2023-12-08 17:13:02 +0400

    feat: Integrate React Router for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Account from "./pages/Account";
+import Profile from "./features/account/profile/Profile";
+import Links from "./features/account/links/Links";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="account" element={<Account />}>
+          <Route index element={<Navigate replace to="profile" />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="links" element={<Links />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
