6bb6629b8a8060e51aa3f8ae545db4fc0a71fc64
Sandro <vipsandro11@gmail.com>
2023-12-09 13:01:04 +0400

    style: remove border in Account component

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import AccountNav from "../ui/AccountNav";
 const Account = () => {
   return (
-    <main className="section-center border py-[25px]">
+    <main className="section-center py-[25px]">
       <AccountNav />
       <Outlet />
     </main>
