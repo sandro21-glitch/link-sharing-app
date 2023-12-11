f18fafad756414c074ec32a7d84270e044280acf
Sandro <vipsandro11@gmail.com>
2023-12-11 14:26:40 +0400

    refactor: Simplify route structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Account from "./pages/Account";
-import Profile from "./features/account/profile/Profile";
-import Links from "./features/account/links/Links";
+import AccountPage from "./features/account/AccountPage";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="account" element={<Account />}>
-          <Route index element={<Navigate replace to="profile" />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="links" element={<Links />} />
+          <Route index element={<AccountPage />} />
         </Route>
+        <Route index element={<Navigate replace to="account" />} />
       </Routes>
     </BrowserRouter>
   );
