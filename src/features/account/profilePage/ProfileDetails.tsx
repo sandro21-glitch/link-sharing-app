ca78aa885ea24ca442144ec4c0390b774ace325c
Sandro <vipsandro11@gmail.com>
2023-12-14 22:55:35 +0400

    refactor: ProfileDetails component structure

--- src/features/account/profilePage/ProfileDetails.tsx
+++ src/features/account/profilePage/ProfileDetails.tsx
@@ -1,4 +1,6 @@
-import uploadImage from "/assets/images/icon-upload-image.svg";
+import BasicDetails from "./profileDetails/BasicDetails";
+import UploadImage from "./profileDetails/UploadImage";
+
 const ProfileDetails = () => {
   return (
     <form className="bg-white flex-1 rounded-[12px]">
@@ -8,22 +10,8 @@ const ProfileDetails = () => {
       <p className="mb-10 px-10">
         Add your details to create a personal touch to your profile.
       </p>
-      <section className="flex justify-between px-5 items-center gap-5 mx-auto bg-[#fafafa] w-[85%] h-[233px]">
-        <p className=" mx-auto">Profile picture</p>
-        <label
-          htmlFor="inputFile"
-          className="bg-veryLightPurple w-[193px] h-[193px] flex justify-center items-center flex-col rounded-[12px] text-strongPurple font-semibold cursor-pointer"
-        >
-          <input
-            id="inputFile"
-            type="file"
-            accept="image/png, image/jpeg"
-            className="hidden"
-          />
-          <img src={uploadImage} />+ Upload Image
-        </label>
-        <p className="mx-auto w-[155px] text-[.75rem]">Image must be below 1024x1024px. Use PNG or JPG format.</p>
-      </section>
+      <UploadImage />
+      <BasicDetails />
     </form>
   );
 };
