0d5f5c287ef621a86c7f2e5294282f9c90f8cde6
Sandro <vipsandro11@gmail.com>
2023-12-15 23:00:42 +0400

    refactor: replace BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  HashRouter as BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Account from "./pages/Account";
 import AccountPage from "./features/account/AccountPage";
 const App = () => {
