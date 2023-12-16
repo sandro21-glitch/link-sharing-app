5691ace671d784da91c0cb45e253d2c138b9c8fa
Sandro <vipsandro11@gmail.com>
2023-12-16 22:08:45 +0400

    feat: Add SaveBtn component for save button in CustomizeLinks

---
+++ src/features/account/linksPage/customizeLinks/SaveBtn.tsx
@@ -0,0 +1,11 @@
+const SaveBtn = () => {
+  return (
+    <div className="mt-5 border border-transparent border-t-[#d9d9d9] flex justify-end">
+      <button className="text-white mt-5 bg-strongPurple font-semibold capitalize w-[91px] h-[46px] rounded-[8px] hover:opacity-60 transition-opacity ease-in duration-150">
+        Save
+      </button>
+    </div>
+  );
+};
+
+export default SaveBtn;
