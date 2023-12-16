dda578901211008dd88a06231132aa25641525e7
Sandro <vipsandro11@gmail.com>
2023-12-16 22:07:56 +0400

    feat: Add AddNewLink component for 'Add new link' button

---
+++ src/features/account/linksPage/customizeLinks/AddNewLink.tsx
@@ -0,0 +1,9 @@
+const AddNewLink = () => {
+  return (
+    <button className="mb-5 border border-strongPurple w-full py-3 rounded-lg text-strongPurple font-medium hover:bg-veryLightPurple transition-colors ease-in duration-150">
+      + Add new link
+    </button>
+  );
+};
+
+export default AddNewLink;
