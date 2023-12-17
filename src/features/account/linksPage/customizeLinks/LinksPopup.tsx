5e4322ea51fe51b4a74565e93b958e4fef86e1d1
Sandro <vipsandro11@gmail.com>
2023-12-17 23:24:29 +0400

    feat: Implement LinksPopup component

---
+++ src/features/account/linksPage/customizeLinks/LinksPopup.tsx
@@ -0,0 +1,20 @@
+import { LINK_SITES } from "../../../../platforms";
+
+const LinksPopup = () => {
+  return (
+    <div className="absolute z-[9999] border rounded-md top-[5.5rem] bg-white w-full h-[345px] px-[16px] overflow-y-scroll">
+      {LINK_SITES.map((link) => {
+        return (
+          <li className="flex items-center gap-3 border-b py-[12px] cursor-pointer group">
+            <img src={link.logo} alt={link.name} />
+            <span className="text-darkGrey group-hover:text-strongPurple transition-colors ease-in duration-150">
+              {link.name}
+            </span>
+          </li>
+        );
+      })}
+    </div>
+  );
+};
+
+export default LinksPopup;
