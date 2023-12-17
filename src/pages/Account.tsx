95b7e750e79d63411cc0ba8abca431a0404e62ac
Sandro <vipsandro11@gmail.com>
2023-12-17 23:25:23 +0400

    feat: Integrate state management for current page in Account component

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import AccountNav from "../ui/AccountNav";
+import { useState } from "react";
 const Account = () => {
+  const [currentPage, setCurrentPage] = useState("links");
+
   return (
     <main className="section-center py-[25px]">
       <AccountNav />
-      <Outlet />
+      <Outlet context={[currentPage, setCurrentPage]} />
     </main>
   );
 };
