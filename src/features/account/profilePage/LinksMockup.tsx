b80a0268c880db67f9f6ce04dd57803d4ddea7d1
Sandro <vipsandro11@gmail.com>
2023-12-18 17:18:44 +0400

    refactor: Integrate LinkBoxes into LinksMockup for enhanced functionality

--- src/features/account/profilePage/LinksMockup.tsx
+++ src/features/account/profilePage/LinksMockup.tsx
@@ -1,13 +1,16 @@
-import phoneMockup from '/assets/images/illustration-phone-mockup.svg'
+import LinkBoxes from "../linkBoxesContainer/LinkBoxes";
+import phoneMockup from "/assets/images/illustration-phone-mockup.svg";
 const LinksMockup = () => {
   return (
-    <section className="bg-white rounded-[12px] w-[512px] flex-center h-[834px]">
-      <div>
+    <section className="bg-white rounded-[12px] w-[512px] h-[834px] flex-center ">
+      <div className="relative">
         <img
           src={phoneMockup}
           alt="phoneMockup"
           className="min-w-[307px] h-auto object-contain"
         />
+        <div className="absolute bottom-[57px] w-[237px] h-[300px] bg-white left-0 right-0 mx-auto"></div>
+        <LinkBoxes />
       </div>
     </section>
   );
