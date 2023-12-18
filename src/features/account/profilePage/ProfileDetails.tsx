2948016b39caf746750b1edf4b3b9a76f8d65234
Sandro <vipsandro11@gmail.com>
2023-12-18 17:18:58 +0400

    feat: Extend ProfileDetails form with Save button

--- src/features/account/profilePage/ProfileDetails.tsx
+++ src/features/account/profilePage/ProfileDetails.tsx
@@ -12,6 +12,14 @@ const ProfileDetails = () => {
       </p>
       <UploadImage />
       <BasicDetails />
+      <div className="mt-5 border border-transparent border-t-[#d9d9d9] flex justify-end">
+        <button
+          type="submit"
+          className="text-white mt-5 bg-strongPurple font-semibold capitalize w-[91px] h-[46px] rounded-[8px] hover:opacity-60 transition-opacity ease-in duration-150"
+        >
+          Save
+        </button>
+      </div>
     </form>
   );
 };
