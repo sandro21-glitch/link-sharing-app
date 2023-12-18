7db4efea6a449432fda78f29b76d7f82ae6b6081
Sandro <vipsandro11@gmail.com>
2023-12-18 17:17:34 +0400

    feat: Introduce LinkBoxes component with SingleLinkBox

---
+++ src/features/account/linkBoxesContainer/LinkBoxes.tsx
@@ -0,0 +1,12 @@
+import SingleLinkBox from "./SingleLinkBox";
+
+const LinkBoxes = () => {
+  return (
+    <div className="absolute top-[278px] left-0 right-0 mx-auto w-[237px] flex flex-col gap-[20px]">
+      {/* single box */}
+      <SingleLinkBox />
+    </div>
+  );
+};
+
+export default LinkBoxes;
