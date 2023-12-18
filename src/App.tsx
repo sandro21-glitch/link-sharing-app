0b50d22e17c38f357f3019d2eb7134fbcafe4d63
Sandro <vipsandro11@gmail.com>
2023-12-18 17:16:50 +0400

    refactor: Simplify React Router import and component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import {
-  HashRouter as BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 import Account from "./pages/Account";
-import AccountPage from "./features/account/AccountPage";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="account" element={<Account />}>
-          <Route index element={<AccountPage />} />
-        </Route>
-        <Route index element={<Navigate replace to="account" />} />
+        <Route path="account" element={<Account />} />
       </Routes>
     </BrowserRouter>
   );
