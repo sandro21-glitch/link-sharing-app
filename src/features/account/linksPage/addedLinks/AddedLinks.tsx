baca94b1bbb11fbb8d278e64e578489e22278d2a
Sandro <vipsandro11@gmail.com>
2023-12-18 17:18:05 +0400

    refactor: Embed LinkBoxes in AddedLinks component

--- src/features/account/linksPage/addedLinks/AddedLinks.tsx
+++ src/features/account/linksPage/addedLinks/AddedLinks.tsx
@@ -1,14 +1,16 @@
+import LinkBoxes from "../../linkBoxesContainer/LinkBoxes";
 import phoneMockup from "/assets/images/illustration-phone-mockup.svg";
-
 const AddedLinks = () => {
   return (
-    <section className="bg-white rounded-[12px] w-[512px] flex-center">
-      <div>
+    <section className="bg-white rounded-[12px] w-[512px] h-[834px] flex-center">
+      <div className="relative
+      ">
         <img
           src={phoneMockup}
           alt="phoneMockup"
           className="min-w-[307px] h-auto object-contain"
         />
+        <LinkBoxes />
       </div>
     </section>
   );
