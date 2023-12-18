a8627206e05cdb0ec2c549790089066676dadf90
Sandro <vipsandro11@gmail.com>
2023-12-18 17:19:28 +0400

    refactor: Enhance AccountNav with dynamic icons and state management

--- src/ui/AccountNav.tsx
+++ src/ui/AccountNav.tsx
@@ -1,7 +1,11 @@
 import logoLarge from "/assets/images/logo-devlinks-large.svg";
-import link from "/assets/images/icon-links-header.svg";
-import profile from "/assets/images/icon-profile-details-header.svg";
-const AccountNav = () => {
+import LinkIcon from "./LinkIcon";
+import ProfileIcon from "./ProfileIcon";
+type AccountNavTypes = {
+  currPage: string;
+  setCurrPage: (page: string) => void;
+};
+const AccountNav = ({ currPage, setCurrPage }: AccountNavTypes) => {
   return (
     <nav className="w-full h-[78px] p-5 bg-white rounded-md flex items-center justify-between mb-5">
       <img
@@ -10,14 +14,40 @@ const AccountNav = () => {
         className="w-[146px] h-auto object-contain"
       />
       <ul className="flex items-center gap-8">
-        <li className="w-[122px] h-[46px] bg-veryLightPurple rounded-lg flex justify-center items-center gap-2 cursor-pointer hover:bg-veryLightPurple transition-colors ease-in duration-150">
-          <img src={link} alt="link" />
-          <span className="text-strongPurple font-semibold">Links</span>
+        <li
+          onClick={() => setCurrPage("links")}
+          className={`w-[122px] h-[46px] ${
+            currPage === "links" ? "bg-veryLightPurple" : "bg-none"
+          }  rounded-lg flex justify-center items-center gap-2
+           cursor-pointer hover:bg-veryLightPurple transition-colors ease-in duration-150`}
+        >
+          <LinkIcon fill={currPage === "links" ? "#633CFF" : "#737373"} />
+          <span
+            className={`${
+              currPage === "links" ? "text-strongPurple" : "text-darkGrey"
+            }  font-semibold`}
+          >
+            Links
+          </span>
         </li>
-        <li className="w-[187px] h-[46px] rounded-lg flex justify-center items-center gap-2 cursor-pointer hover:bg-veryLightPurple transition-colors ease-in duration-150">
+        <li
+          onClick={() => setCurrPage("profile")}
+          className={`w-[187px] h-[46px] ${
+            currPage === "profile" ? "bg-veryLightPurple" : "bg-none"
+          }  rounded-lg flex justify-center items-center gap-2
+           cursor-pointer hover:bg-veryLightPurple transition-colors ease-in duration-150`}
+        >
           <div className="flex items-center gap-2">
-            <img src={profile} alt="profile" />
-            <span className="text-darkGrey font-semibold">Profile Details</span>
+            <ProfileIcon
+              fill={currPage === "profile" ? "#633CFF" : "#737373"}
+            />
+            <span
+              className={`${
+                currPage === "profile" ? "text-strongPurple" : "text-darkGrey"
+              }  font-semibold`}
+            >
+              Profile Details
+            </span>
           </div>
         </li>
       </ul>
