5969ad78086257e8d6edcb43e0b89b9dd8140c92
Sandro <vipsandro11@gmail.com>
2023-12-18 17:18:26 +0400

    refactor: Add key to LinksPopup list items for better React rendering

--- src/features/account/linksPage/customizeLinks/LinksPopup.tsx
+++ src/features/account/linksPage/customizeLinks/LinksPopup.tsx
@@ -3,9 +3,12 @@ import { LINK_SITES } from "../../../../platforms";
 const LinksPopup = () => {
   return (
     <div className="absolute z-[9999] border rounded-md top-[5.5rem] bg-white w-full h-[345px] px-[16px] overflow-y-scroll">
-      {LINK_SITES.map((link) => {
+      {LINK_SITES.map((link, index) => {
         return (
-          <li className="flex items-center gap-3 border-b py-[12px] cursor-pointer group">
+          <li
+            key={index}
+            className="flex items-center gap-3 border-b py-[12px] cursor-pointer group"
+          >
             <img src={link.logo} alt={link.name} />
             <span className="text-darkGrey group-hover:text-strongPurple transition-colors ease-in duration-150">
               {link.name}
