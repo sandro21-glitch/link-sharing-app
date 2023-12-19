9d5b6fed291f94b7d4ae68ed88deaf1617963e59
Sandro <vipsandro11@gmail.com>
2023-12-19 21:21:15 +0400

    feat: :zap: refactor: enhance accountReducer with type-safe action and payload
    Updated the accountReducer to utilize type-safe actions by introducing the AddNewLinkAction interface. This ensures that the payload associated with the ADD_NEW_LINK action is correctly typed as LinkType, improving code clarity and maintainability.

--- src/reducers/accountReducer.ts
+++ src/reducers/accountReducer.ts
@@ -1,5 +1,18 @@
+import { ADD_NEW_LINK } from "../actions/accountActions";
 import { accountStateTypes } from "../context/accountContext";
+import { LinkType } from "../types/linksType";
 
-export const accountReducer = (state: accountStateTypes) => {
+interface AddNewLinkAction {
+  type: typeof ADD_NEW_LINK;
+  payload: LinkType;
+}
+
+export const accountReducer = (
+  state: accountStateTypes,
+  action: AddNewLinkAction
+): accountStateTypes => {
+  if (action.type === ADD_NEW_LINK) {
+    return { ...state, links: [...state.links, action.payload] };
+  }
   return state;
 };
