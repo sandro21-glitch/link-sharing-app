19692e9439edc5ccfaf5952078ed12b23827cb66
Sandro <vipsandro11@gmail.com>
2023-12-19 21:17:17 +0400

    feat: :sparkles: feat: enhance AddNewLink component with dynamic link creation
    Refactored the AddNewLink component to utilize the useAccountContext hook for accessing the account context. Now, the component dynamically creates a new link (GitHub in this case) with the relevant information, such as name, placeholder URL, logo, color, and validation. When the "+ Add new link" button is clicked, the addNewLink function from the context is invoked with the newly created link, allowing for dynamic addition of links.

--- src/features/account/linksPage/customizeLinks/AddNewLink.tsx
+++ src/features/account/linksPage/customizeLinks/AddNewLink.tsx
@@ -1,6 +1,20 @@
+import { useAccountContext } from "../../../../context/accountContext";
+import GitHubLogo from "/assets/images/icon-github.svg";
 const AddNewLink = () => {
+  const { addNewLink } = useAccountContext();
+  const tempLink = {
+    name: "GitHub",
+    placeholderUrl: "https://www.github.com/johnappleseed",
+    logo: GitHubLogo,
+    color: "hsl(0, 0%, 10%)",
+    validation: /(https:\/\/)?(www\.)?github\.com\/[\w\-\/]*/,
+  };
   return (
-    <button className="mb-5 border border-strongPurple w-full py-3 rounded-lg text-strongPurple font-medium hover:bg-veryLightPurple transition-colors ease-in duration-150">
+    <button
+      type="button"
+      onClick={() => addNewLink(tempLink)}
+      className="mb-5 border border-strongPurple w-full py-3 rounded-lg text-strongPurple font-medium hover:bg-veryLightPurple transition-colors ease-in duration-150"
+    >
       + Add new link
     </button>
   );
