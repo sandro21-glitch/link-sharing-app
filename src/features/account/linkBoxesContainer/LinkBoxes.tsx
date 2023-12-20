72ecd35ab0ccb12981a7706dca3157580952f624
Sandro <vipsandro11@gmail.com>
2023-12-20 23:15:47 +0400

    refactor: :sparkles: integrate useAccountContext in LinkBoxes component
    In LinkBoxes component, replace static SingleLinkBox with a dynamic rendering based on the links obtained from the useAccountContext hook. Each link is now rendered using the SingleLinkBox component. This change enhances the flexibility and reusability of the LinkBoxes component.

--- src/features/account/linkBoxesContainer/LinkBoxes.tsx
+++ src/features/account/linkBoxesContainer/LinkBoxes.tsx
@@ -1,10 +1,15 @@
+import { useAccountContext } from "../../../context/AccountContext";
 import SingleLinkBox from "./SingleLinkBox";
 
 const LinkBoxes = () => {
+  const { state } = useAccountContext();
+  const links = state.links;
   return (
     <div className="absolute top-[278px] left-0 right-0 mx-auto w-[237px] flex flex-col gap-[20px]">
       {/* single box */}
-      <SingleLinkBox />
+      {links.map((link, index) => {
+        return <SingleLinkBox key={index} link={link} />;
+      })}
     </div>
   );
 };
