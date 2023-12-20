4590e59aa4371b799181fcb317cd77d689356f4a
Sandro <vipsandro11@gmail.com>
2023-12-20 23:14:47 +0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Account from "./pages/Account";
 const App = () => {
   return (
