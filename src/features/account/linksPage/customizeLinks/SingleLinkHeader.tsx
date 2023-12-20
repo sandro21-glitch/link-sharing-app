fe21dab848017d99175c54773cd6224564bcb1cb
Sandro <vipsandro11@gmail.com>
2023-12-20 23:18:55 +0400

    refactor: :sparkles: enhance SingleLinkHeader to dynamically display link index

--- src/features/account/linksPage/customizeLinks/SingleLinkHeader.tsx
+++ src/features/account/linksPage/customizeLinks/SingleLinkHeader.tsx
@@ -1,7 +1,12 @@
-const SingleLinkHeader = () => {
+type LinkHeaderTypes = {
+  index: number;
+};
+const SingleLinkHeader = ({ index }: LinkHeaderTypes) => {
   return (
     <header className="w-full flex justify-between items-center mb-3">
-      <h1 className="text-[1rem] font-semibold text-darkGrey">= Link #1</h1>
+      <h1 className="text-[1rem] font-semibold text-darkGrey">
+        = Link #{index + 1}
+      </h1>
       <button className="text-darkGrey hover:text-strongPurple">Remove</button>
     </header>
   );
