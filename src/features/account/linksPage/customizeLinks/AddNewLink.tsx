fdc36b13cc361270a5c2110c627d579f4d303cba
Sandro <vipsandro11@gmail.com>
2023-12-21 22:05:34 +0400

    refactor: refactor: update AddNewLink to use predefined link from LINK_SITES

--- src/features/account/linksPage/customizeLinks/AddNewLink.tsx
+++ src/features/account/linksPage/customizeLinks/AddNewLink.tsx
@@ -1,18 +1,14 @@
-import { useAccountContext } from "../../../../context/accountContext";
-import GitHubLogo from "/assets/images/icon-github.svg";
+import { useAccountContext } from "../../../../context/AccountContext";
+// import GitHubLogo from "/assets/images/icon-github.svg";
+// import GitHubLogoWhite from "/assets/images/icon-github-link-box.svg";
+import { LINK_SITES } from "../../../../platforms";
 const AddNewLink = () => {
   const { addNewLink } = useAccountContext();
-  const tempLink = {
-    name: "GitHub",
-    placeholderUrl: "https://www.github.com/johnappleseed",
-    logo: GitHubLogo,
-    color: "hsl(0, 0%, 10%)",
-    validation: /(https:\/\/)?(www\.)?github\.com\/[\w\-\/]*/,
-  };
+
   return (
     <button
       type="button"
-      onClick={() => addNewLink(tempLink)}
+      onClick={() => addNewLink(LINK_SITES[5])}
       className="mb-5 border border-strongPurple w-full py-3 rounded-lg text-strongPurple font-medium hover:bg-veryLightPurple transition-colors ease-in duration-150"
     >
       + Add new link
