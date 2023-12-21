1e6ab6192fc9880b349100ecf24571edd2288b59
Sandro <vipsandro11@gmail.com>
2023-12-21 22:03:25 +0400

    initial commit

--- src/platforms.tsx
+++ src/platforms.tsx
@@ -3,21 +3,34 @@ import GitHubLogoWhite from "/assets/images/icon-github-link-box.svg";
 import YouTubeLogo from "/assets/images/icon-youtube.svg";
 import YouTubeLogoWhit from "/assets/images/icon-youtube-link-box.svg";
 import TwitterLogo from "/assets/images/icon-twitter.svg";
+import TwitterLogoWhite from "/assets/images/icon-twitter-link-box.svg";
 import LinkedInLogo from "/assets/images/icon-linkedin.svg";
+import LinkedInLogoWhite from "/assets/images/icon-linkedin-link-box.svg";
 import FacebookLogo from "/assets/images/icon-facebook.svg";
+import FacebookLogoWhite from "/assets/images/icon-facebook-link-box.svg";
 import FrontendMentorLogo from "/assets/images/icon-frontend-mentor.svg";
+import FrontendMentorLogoWhite from "/assets/images/icon-frontend-mentor-link-box.svg";
 import TwitchLogo from "/assets/images/icon-twitch.svg";
+import TwitchLogoWhite from "/assets/images/icon-twitch-link-box.svg";
 import DevtoLogo from "/assets/images/icon-devto.svg";
+import DevtoLogoWhite from "/assets/images/icon-devto-link-box.svg";
 import CodewarsLogo from "/assets/images/icon-codewars.svg";
+import CodewarsLogoWhite from "/assets/images/icon-codewars-link-box.svg";
 import CodepenLogo from "/assets/images/icon-codepen.svg";
+import CodepenLogoWhite from "/assets/images/icon-codepen-link-box.svg";
 import FreeCodeCampLogo from "/assets/images/icon-freecodecamp.svg";
+import FreeCodeCampLogoWhite from "/assets/images/icon-freecodecamp-link-box.svg";
 import GitLabLogo from "/assets/images/icon-gitlab.svg";
+import GitLabLogoWhite from "/assets/images/icon-gitlab-link-box.svg";
 import HashNodeLogo from "/assets/images/icon-hashnode.svg";
+import HashNodeLogoWhite from "/assets/images/icon-hashnode-link-box.svg";
 import StackOverflowLogo from "/assets/images/icon-stack-overflow.svg";
+import StackOverflowLogoWhite from "/assets/images/icon-stack-overflow-link-box.svg";
 type LinksTypes = {
   name: string;
   placeholderUrl: string;
   logo: string;
+  logoWhite: string;
   color: string;
   validation: RegExp;
 }[];
@@ -26,6 +39,7 @@ export const LINK_SITES: LinksTypes = [
     name: "GitHub",
     placeholderUrl: "https://www.github.com/johnappleseed",
     logo: GitHubLogo,
+    logoWhite: GitHubLogoWhite,
     color: "hsl(0, 0%, 1.5686274509803921%)",
     validation: /(https:\/\/)?(www\.)?github\.com\/[\w\-\/]*/,
   },
@@ -33,6 +47,7 @@ export const LINK_SITES: LinksTypes = [
     name: "YouTube",
     placeholderUrl: "https://www.youtube.com/johnappleseed",
     logo: YouTubeLogo,
+    logoWhite: YouTubeLogoWhit,
     color: "hsl(0, 84%, 58%)",
     validation: /(https:\/\/)?(www\.)?youtube\.com\/.*/,
   },
@@ -40,6 +55,7 @@ export const LINK_SITES: LinksTypes = [
     name: "Twitter",
     placeholderUrl: "https://www.twitter.com/johnappleseed",
     logo: TwitterLogo,
+    logoWhite: TwitterLogoWhite,
     color: "hsl(198, 79%, 59%)",
     validation: /(https:\/\/)?(www\.)?twitter\.com\/[\w\-\/]*/,
   },
@@ -47,6 +63,7 @@ export const LINK_SITES: LinksTypes = [
     name: "LinkedIn",
     placeholderUrl: "https://www.linkedin.com/johnappleseed",
     logo: LinkedInLogo,
+    logoWhite: LinkedInLogoWhite,
     color: "hsl(223, 100%, 59%)",
     validation: /(https:\/\/)?(www\.)?linkedin\.com\/[\w\-\/]*/,
   },
@@ -54,6 +71,7 @@ export const LINK_SITES: LinksTypes = [
     name: "Facebook",
     placeholderUrl: "https://www.facebook.com/johnappleseed",
     logo: FacebookLogo,
+    logoWhite: FacebookLogoWhite,
     color: "hsl(227, 66%, 41%)",
     validation: /(https:\/\/)?(www\.)?facebook\.com\/[\w\-\/]*/,
   },
@@ -61,6 +79,7 @@ export const LINK_SITES: LinksTypes = [
     name: "Frontend Mentor",
     placeholderUrl: "https://www.frontendmentor.io/johnappleseed",
     logo: FrontendMentorLogo,
+    logoWhite: FrontendMentorLogoWhite,
     color: "hsl(0, 0%, 100%)",
     validation: /(https:\/\/)?(www\.)?frontendmentor\.io\/[\w\-\/]*/,
   },
@@ -68,6 +87,7 @@ export const LINK_SITES: LinksTypes = [
     name: "Twitch",
     placeholderUrl: "https://www.twitch.tv/johnappleseed",
     logo: TwitchLogo,
+    logoWhite: TwitchLogoWhite,
     color: "hsl(313, 84%, 59%)",
     validation: /(https:\/\/)?(www\.)?twitch\.tv\/[\w\-\/]*/,
   },
@@ -75,6 +95,7 @@ export const LINK_SITES: LinksTypes = [
     name: "Dev.to",
     placeholderUrl: "https://dev.to/johnappleseed",
     logo: DevtoLogo,
+    logoWhite: DevtoLogoWhite,
     color: "hsl(0, 0%, 20%)",
     validation: /(https:\/\/)?(www\.)?dev\.to\/[\w\-\/]*/,
   },
@@ -82,6 +103,7 @@ export const LINK_SITES: LinksTypes = [
     name: "Codewars",
     placeholderUrl: "https://www.codewars.com/users/johnappleseed",
     logo: CodewarsLogo,
+    logoWhite: CodewarsLogoWhite,
     color: "hsl(331, 68%, 32%)",
     validation: /(https:\/\/)?(www\.)?codewars\.com\/users\/[\w\-\/]*/,
   },
@@ -89,6 +111,7 @@ export const LINK_SITES: LinksTypes = [
     name: "Codepen",
     placeholderUrl: "https://codepen.io/johnappleseed",
     logo: CodepenLogo,
+    logoWhite: CodepenLogoWhite,
     color: "hsl(145, 68%, 32%)",
     validation: /(https:\/\/)?(www\.)?codepen\.io\/[\w\-\/]*/,
   },
@@ -96,6 +119,7 @@ export const LINK_SITES: LinksTypes = [
     name: "freeCodeCamp",
     placeholderUrl: "https://www.freecodecamp.org/johnappleseed",
     logo: FreeCodeCampLogo,
+    logoWhite: FreeCodeCampLogoWhite,
     color: "hsl(252, 50%, 27%)",
     validation: /(https:\/\/)?(www\.)?freecodecamp\.org\/[\w\-\/]*/,
   },
@@ -103,6 +127,7 @@ export const LINK_SITES: LinksTypes = [
     name: "GitLab",
     placeholderUrl: "https://gitlab.com/johnappleseed",
     logo: GitLabLogo,
+    logoWhite: GitLabLogoWhite,
     color: "hsl(11, 83%, 53%)",
     validation: /(https:\/\/)?(www\.)?gitlab\.com\/[\w\-\/]*/,
   },
@@ -110,6 +135,7 @@ export const LINK_SITES: LinksTypes = [
     name: "Hashnode",
     placeholderUrl: "https://hashnode.com/@johnappleseed",
     logo: HashNodeLogo,
+    logoWhite: HashNodeLogoWhite,
     color: "hsl(227, 98%, 42%)",
     validation: /(https:\/\/)?(www\.)?hashnode\.com\/@[\w\-\/]*/,
   },
@@ -117,6 +143,7 @@ export const LINK_SITES: LinksTypes = [
     name: "Stack Overflow",
     placeholderUrl: "https://stackoverflow.com/users/1234/johnappleseed",
     logo: StackOverflowLogo,
+    logoWhite: StackOverflowLogoWhite,
     color: "hsl(29, 100%, 46%)",
     validation:
       /(https:\/\/)?(www\.)?stackoverflow\.com\/users\/\d+\/[\w\-\/]*/,
