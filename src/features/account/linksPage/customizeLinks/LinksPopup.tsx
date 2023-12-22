e7095e149e228b19fff30f8ce65db5c005e54366
Sandro <vipsandro11@gmail.com>
2023-12-22 17:16:27 +0400

    feat: integrate link selection in LinksPopup

--- src/features/account/linksPage/customizeLinks/LinksPopup.tsx
+++ src/features/account/linksPage/customizeLinks/LinksPopup.tsx
@@ -1,11 +1,17 @@
+import { useAccountContext } from "../../../../context/AccountContext";
 import { LINK_SITES } from "../../../../platforms";
+type LinkPopupTypes = {
+  selectIndex: number;
+};
+const LinksPopup = ({ selectIndex }: LinkPopupTypes) => {
+  const { selectLink } = useAccountContext();
 
-const LinksPopup = () => {
   return (
     <div className="absolute z-[9999] border rounded-md top-[5.5rem] bg-white w-full h-[345px] px-[16px] overflow-y-scroll">
       {LINK_SITES.map((link, index) => {
         return (
           <li
+            onClick={() => selectLink(selectIndex)}
             key={index}
             className="flex items-center gap-3 border-b py-[12px] cursor-pointer group"
           >
