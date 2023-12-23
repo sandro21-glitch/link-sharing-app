29819aaf3f698305aaf3778795dc120e40066a93
Sandro <vipsandro11@gmail.com>
2023-12-23 21:39:27 +0400

    feat: :sparkles: Redirect root path to the 'account' page on initial load

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Account from "./pages/Account";
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/account" />} />
         <Route path="account" element={<Account />} />
       </Routes>
     </BrowserRouter>
