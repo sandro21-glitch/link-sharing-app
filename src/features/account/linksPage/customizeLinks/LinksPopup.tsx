4ec1d7a0df3475de7d450115d692495505cbfc77
Sandro <vipsandro11@gmail.com>
2023-12-25 17:13:24 +0400

    initial commit

--- src/features/account/linksPage/customizeLinks/LinksPopup.tsx
+++ src/features/account/linksPage/customizeLinks/LinksPopup.tsx
@@ -1,27 +1,27 @@
 import { useAccountContext } from "../../../../context/AccountContext";
 import { LINK_SITES } from "../../../../platforms";
-type LinkPopupTypes = {
-  selectIndex: number;
-};
-const LinksPopup = ({ selectIndex }: LinkPopupTypes) => {
+import { LinkType } from "../../../../types/linksType";
+const LinksPopup = () => {
   const { selectLink } = useAccountContext();
+
+  const handleLinkClick = (link: LinkType) => {
+    selectLink(link);
+  };
 
   return (
     <div className="absolute z-[9999] border rounded-md top-[5.5rem] bg-white w-full h-[345px] px-[16px] overflow-y-scroll">
-      {LINK_SITES.map((link, index) => {
-        return (
-          <li
-            onClick={() => selectLink(selectIndex)}
-            key={index}
-            className="flex items-center gap-3 border-b py-[12px] cursor-pointer group"
-          >
-            <img src={link.logo} alt={link.name} />
-            <span className="text-darkGrey group-hover:text-strongPurple transition-colors ease-in duration-150">
-              {link.name}
-            </span>
-          </li>
-        );
-      })}
+      {LINK_SITES.map((link) => (
+        <li
+          onClick={() => handleLinkClick(link)}
+          key={link.id}
+          className="flex items-center gap-3 border-b py-[12px] cursor-pointer group"
+        >
+          <img src={link.logo} alt={link.name} />
+          <span className="text-darkGrey group-hover:text-strongPurple transition-colors ease-in duration-150">
+            {link.name}
+          </span>
+        </li>
+      ))}
     </div>
   );
 };
