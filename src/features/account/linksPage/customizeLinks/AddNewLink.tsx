816979858ed51298bea7485e96d07549e7008be0
Sandro <vipsandro11@gmail.com>
2023-12-25 17:13:22 +0400

    initial commit

--- src/features/account/linksPage/customizeLinks/AddNewLink.tsx
+++ src/features/account/linksPage/customizeLinks/AddNewLink.tsx
@@ -8,7 +8,7 @@ const AddNewLink = () => {
   return (
     <button
       type="button"
-      onClick={() => addNewLink(LINK_SITES[5])}
+      onClick={() => addNewLink(LINK_SITES[0])}
       className="mb-5 border border-strongPurple w-full py-3 rounded-lg text-strongPurple font-medium hover:bg-veryLightPurple transition-colors ease-in duration-150"
     >
       + Add new link
