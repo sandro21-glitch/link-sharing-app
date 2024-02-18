e2dcfbd99791a387fea06af9e709066350a564f9
Sandro <vipsandro11@gmail.com>
2024-02-18 20:45:13 +0400

    feat: Create linksSlice in Redux store with initial state and LinkTypes

--- src/features/links.ts
+++ src/features/links.ts
@@ -1,10 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 // import type { PayloadAction } from "@reduxjs/toolkit";
 // import type { RootState } from "../store";
+import { LinkTypes } from "../types/linksType";
+import GitLabLogo from "/assets/images/icon-gitlab.svg";
+import GitLabLogoWhite from "/assets/images/icon-gitlab-link-box.svg";
+interface LinksState {
+  links: LinkTypes[];
+}
 
-interface CounterState {}
-
-const initialState: CounterState = {};
+const initialState: LinksState = {
+  links: [
+    {
+      id: 1,
+      name: "GitHub",
+      placeholderUrl: "https://www.github.com/johnappleseed",
+      logo: GitLabLogo,
+      logoWhite: GitLabLogoWhite,
+      color: "hsl(0, 0%, 1.5686274509803921%)",
+      validation: /(https:\/\/)?(www\.)?github\.com\/[\w\-\/]*/,
+    },
+  ],
+};
 
 export const linksSlice = createSlice({
   name: "links",
