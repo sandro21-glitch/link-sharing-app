c17c6638b78e87588f7dd668b007d0afca013119
Sandro <vipsandro11@gmail.com>
2024-02-18 20:45:48 +0400

    refactor: Enhance SingleLinkBox component with dynamic data

--- src/features/account/linkBoxesContainer/SingleLinkBox.tsx
+++ src/features/account/linkBoxesContainer/SingleLinkBox.tsx
@@ -1,16 +1,27 @@
 import arrowRight from "/assets/images/icon-arrow-right-white.svg";
-
-const SingleLinkBox = () => {
+type SingleLinkTypes = {
+  link: {
+    id: number;
+    name: string;
+    placeholderUrl: string;
+    logo: string;
+    logoWhite: string;
+    color: string;
+    validation: RegExp;
+  };
+};
+const SingleLinkBox = ({ link }: SingleLinkTypes) => {
+  const { color, logo, name } = link;
   return (
     <div
       style={{
-        backgroundColor: "red",
+        backgroundColor: color,
       }}
       className={`border flex justify-between items-center cursor-grab h-[44px] w-full rounded-[8px] text-white px-[16px] text-[.75rem]`}
     >
-      <div>
-        <img src="link.logo" srcSet="link.name" />
-        link name
+      <div className="flex gap-2 items-center">
+        <img src={logo} srcSet={name} />
+        <span>{name}</span>
       </div>
       <img src={arrowRight} alt="arrow right" />
     </div>
