a6c3deb0e66eeaecc6f321416c4bee9d7f69db93
Sandro <vipsandro11@gmail.com>
2024-02-18 20:46:08 +0400

    style: Minor formatting adjustment in AddedLinks component

--- src/features/account/linksPage/addedLinks/AddedLinks.tsx
+++ src/features/account/linksPage/addedLinks/AddedLinks.tsx
@@ -3,8 +3,10 @@ import phoneMockup from "/assets/images/illustration-phone-mockup.svg";
 const AddedLinks = () => {
   return (
     <section className="bg-white rounded-[12px] w-[512px] h-[834px] flex-center">
-      <div className="relative
-      ">
+      <div
+        className="relative
+      "
+      >
         <img
           src={phoneMockup}
           alt="phoneMockup"
