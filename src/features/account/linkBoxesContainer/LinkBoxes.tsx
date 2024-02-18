fa252eabcaf7f0736848d7663f97374c8714dc51
Sandro <vipsandro11@gmail.com>
2024-02-18 20:45:27 +0400

    feat: Integrate links data into LinkBoxes component

--- src/features/account/linkBoxesContainer/LinkBoxes.tsx
+++ src/features/account/linkBoxesContainer/LinkBoxes.tsx
@@ -1,10 +1,15 @@
+import { useAppSelector } from "../../../hooks/reduxHooks";
 import SingleLinkBox from "./SingleLinkBox";
 
 const LinkBoxes = () => {
+  const { links } = useAppSelector((store) => store.links);
   return (
     <div className="absolute top-[278px] left-0 right-0 mx-auto w-[237px] flex flex-col gap-[20px]">
       {/* single box */}
-      <SingleLinkBox />;
+      {links.map((link) => {
+        const { id } = link;
+        return <SingleLinkBox key={id} link={link} />;
+      })}
     </div>
   );
 };
