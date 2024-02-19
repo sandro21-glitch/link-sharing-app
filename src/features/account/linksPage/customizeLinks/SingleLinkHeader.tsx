76d3fca94cac0ff8d9dc945eddeaa794b272ade1
Sandro <vipsandro11@gmail.com>
2024-02-19 23:30:46 +0400

    feat: add link removal functionality in SingleLinkHeader

--- src/features/account/linksPage/customizeLinks/SingleLinkHeader.tsx
+++ src/features/account/linksPage/customizeLinks/SingleLinkHeader.tsx
@@ -1,13 +1,21 @@
+import { useAppDispatch } from "../../../../hooks/reduxHooks";
+import { removeLink } from "../../../links";
 type LinkHeaderTypes = {
-  id: number;
+  id: string;
 };
 const SingleLinkHeader = ({ id }: LinkHeaderTypes) => {
+  const dispatch = useAppDispatch();
+
   return (
     <header className="w-full flex justify-between items-center mb-3">
-      <h1 className="text-[1rem] font-semibold text-darkGrey">
-        = Link #{id}
-      </h1>
-      <button className="text-darkGrey hover:text-strongPurple">Remove</button>
+      <h1 className="text-[1rem] font-semibold text-darkGrey">= Link #1</h1>
+      <button
+        type="button"
+        onClick={() => dispatch(removeLink(id))}
+        className="text-darkGrey hover:text-strongPurple"
+      >
+        Remove
+      </button>
     </header>
   );
 };
