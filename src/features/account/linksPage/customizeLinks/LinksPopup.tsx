4ebed75be7a36ecd72cc5c1b723f4e2158dc5df4
Sandro <vipsandro11@gmail.com>
2024-02-19 20:11:15 +0400

    feat: Integrate link editing functionality in LinksPopup component

--- src/features/account/linksPage/customizeLinks/LinksPopup.tsx
+++ src/features/account/linksPage/customizeLinks/LinksPopup.tsx
@@ -1,13 +1,25 @@
+import { useAppDispatch } from "../../../../hooks/reduxHooks";
 import { LINK_SITES } from "../../../../platforms";
-
-const LinksPopup = () => {
+import { SingleLinkType } from "../../../../types/linksType";
+import { editLink } from "../../../links";
+type LinkPopupTypes = {
+  id: string;
+  setOpenPopup: (open: boolean) => void;
+};
+const LinksPopup = ({ id, setOpenPopup }: LinkPopupTypes) => {
+  console.log(id);
+  const dispatch = useAppDispatch();
+  const handleChangeLink = (link: SingleLinkType) => {
+    dispatch(editLink({ id, link }));
+    setOpenPopup(false);
+  };
   return (
     <ul className="absolute z-[9999] border rounded-md top-[5.5rem] bg-white w-full h-[345px] px-[16px] overflow-y-scroll">
       {LINK_SITES.map((link) => {
         const { id, name, logo } = link;
         return (
           <li
-            // onClick={() => handleChangeLink(link)}
+            onClick={() => handleChangeLink(link)}
             key={id}
             className="flex items-center gap-3 border-b py-[12px] cursor-pointer group"
           >
