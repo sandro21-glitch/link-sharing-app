608b816ff0b20782f51ef69c27a601dadfd4ee4a
Sandro <vipsandro11@gmail.com>
2024-02-19 20:10:55 +0400

    style: Update SingleLinkBox component

--- src/features/account/linkBoxesContainer/SingleLinkBox.tsx
+++ src/features/account/linkBoxesContainer/SingleLinkBox.tsx
@@ -1,17 +1,19 @@
 import arrowRight from "/assets/images/icon-arrow-right-white.svg";
 type SingleLinkTypes = {
   link: {
-    id: number;
+    id: string;
     name: string;
     placeholderUrl: string;
     logo: string;
     logoWhite: string;
     color: string;
-    validation: RegExp;
+    validation: string;
   };
 };
 const SingleLinkBox = ({ link }: SingleLinkTypes) => {
   const { color, logo, name } = link;
+  console.log(logo);
+
   return (
     <div
       style={{
@@ -20,7 +22,7 @@ const SingleLinkBox = ({ link }: SingleLinkTypes) => {
       className={`border flex justify-between items-center cursor-grab h-[44px] w-full rounded-[8px] text-white px-[16px] text-[.75rem]`}
     >
       <div className="flex gap-2 items-center">
-        <img src={logo} srcSet={name} />
+        <img src={logo} alt={name} />
         <span>{name}</span>
       </div>
       <img src={arrowRight} alt="arrow right" />
