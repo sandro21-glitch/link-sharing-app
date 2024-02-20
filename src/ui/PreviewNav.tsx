e1a97e437edd7e2549381267a0158109e9350b78
Sandro <vipsandro11@gmail.com>
2024-02-20 19:07:25 +0400

    feat: Added navigation to go to the account page on button

--- src/ui/PreviewNav.tsx
+++ src/ui/PreviewNav.tsx
@@ -1,8 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 const PreviewNav = () => {
+  const navigate = useNavigate();
   return (
     <nav className="max-w-[1264px] w-full mx-auto h-[78px] p-5 bg-white rounded-lg flex items-center justify-between mb-5">
       <div className="flex justify-between w-full">
         <button
+          type="button"
+          onClick={() => navigate("/")}
           className="border-strongPurple border w-[159px] h-[46px]
          rounded-md font-semibold text-strongPurple hover:text-white hover:bg-strongPurple transition-all ease-in duration-150"
         >
