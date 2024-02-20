de526790a586d371b3a05a57dfcef8ac99a458b6
Sandro <vipsandro11@gmail.com>
2024-02-20 19:01:52 +0400

    feat: create Preview.tsx

---
+++ src/pages/Preview.tsx
@@ -0,0 +1,13 @@
+import DisplayProfile from "../features/preview/DisplayProfile";
+import PreviewNav from "../ui/PreviewNav";
+
+const Preview = () => {
+  return <main>
+    <header className="h-[357px] w-full bg-strongPurple rounded-b-[2rem] py-[25px] px-5">
+        <PreviewNav />
+        <DisplayProfile />
+    </header>
+  </main>;
+};
+
+export default Preview;
