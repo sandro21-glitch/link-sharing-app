13a1d05aa6ef2f7248e1e09a6b89d705f80420d7
Sandro <vipsandro11@gmail.com>
2024-02-21 19:19:32 +0400

    style: Update SaveBtn component for better accessibility

--- src/features/account/linksPage/customizeLinks/SaveBtn.tsx
+++ src/features/account/linksPage/customizeLinks/SaveBtn.tsx
@@ -1,10 +1,13 @@
 const SaveBtn = () => {
   return (
-    <div className="mt-5 border border-transparent border-t-[#d9d9d9] flex justify-end">
-      <button className="text-white mt-5 bg-strongPurple font-semibold capitalize w-[91px] h-[46px] rounded-[8px] hover:opacity-60 transition-opacity ease-in duration-150">
+    <button
+      type="submit"
+      className="mt-5 w-full  border border-transparent border-t-[#d9d9d9] flex justify-end"
+    >
+      <div className="text-white flex justify-center items-center mt-5 bg-strongPurple font-semibold capitalize w-[91px] h-[46px] rounded-[8px] hover:opacity-60 transition-opacity ease-in duration-150">
         Save
-      </button>
-    </div>
+      </div>
+    </button>
   );
 };
 
