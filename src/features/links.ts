b7208a2bb3d80abc6f3e9f60eeb4518509995638
Sandro <vipsandro11@gmail.com>
2024-02-21 21:29:54 +0400

    feat: Initialize links state with data from localStorage

--- src/features/links.ts
+++ src/features/links.ts
@@ -3,13 +3,14 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import { v4 as uuidv4 } from "uuid";
 // import type { RootState } from "../store";
 import { LinkTypes, SingleLinkType } from "../types/linksType";
+const storedLinks = localStorage.getItem("links");
 
 interface LinksState {
   links: LinkTypes[];
 }
 
 const initialState: LinksState = {
-  links: [],
+  links: storedLinks ? JSON.parse(storedLinks) : [],
 };
 
 export const linksSlice = createSlice({
@@ -19,7 +20,7 @@ export const linksSlice = createSlice({
     addNewLink: (state, action: PayloadAction<SingleLinkType>) => {
       const newLink = {
         id: uuidv4(),
-        link: "",
+        path: "",
         ...action.payload,
       };
       state.links.push(newLink);
@@ -37,15 +38,27 @@ export const linksSlice = createSlice({
         selectedLink.logoWhite = link.logoWhite;
         selectedLink.placeholderUrl = link.placeholderUrl;
         selectedLink.name = link.name;
+        selectedLink.path = "";
       }
     },
     removeLink: (state, action: PayloadAction<string>) => {
       state.links = state.links.filter((link) => link.id !== action.payload);
     },
+    addLinkPath: (
+      state,
+      action: PayloadAction<{ id: string; linkPath: string }>
+    ) => {
+      const { id, linkPath } = action.payload;
+      const link = state.links.find((link) => link.id === id);
+      if (link) {
+        link.path = linkPath;
+      }
+    },
   },
 });
 
-export const { addNewLink, editLink, removeLink } = linksSlice.actions;
+export const { addNewLink, editLink, removeLink, addLinkPath } =
+  linksSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 // export const selectCount = (state: RootState) => state.counter.value;
