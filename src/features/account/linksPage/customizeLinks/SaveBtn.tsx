5cfde320cfdc04210691c1cdbb54ec558234c00a
Sandro <vipsandro11@gmail.com>
2024-02-22 23:14:35 +0400

    feat: Enable SaveBtn to dynamically disable when any link is invalid

--- src/features/account/linksPage/customizeLinks/SaveBtn.tsx
+++ src/features/account/linksPage/customizeLinks/SaveBtn.tsx
@@ -1,8 +1,20 @@
+import { useAppSelector } from "../../../../hooks/reduxHooks";
+
 const SaveBtn = () => {
+  const { links } = useAppSelector((store) => store.links);
+  const areAllLinksValid = links.every((link) => {
+    const patternWithoutSlashes = link.validation.replace(/^\/|\/$/g, "");
+    const regexPattern = new RegExp(patternWithoutSlashes);
+    return regexPattern.test(link.path) || link.path !== "";
+  });
+  console.log("isAnyLinkInvalid:", areAllLinksValid);
   return (
     <button
       type="submit"
-      className="mt-5 w-full  border border-transparent border-t-[#d9d9d9] flex justify-end"
+      disabled={areAllLinksValid ? false : true}
+      className={`${
+        !areAllLinksValid ? "cursor-not-allowed" : "cursor-pointer"
+      } mt-5 w-full  border border-transparent border-t-[#d9d9d9] flex justify-end`}
     >
       <div className="text-white flex justify-center items-center mt-5 bg-strongPurple font-semibold capitalize w-[91px] h-[46px] rounded-[8px] hover:opacity-60 transition-opacity ease-in duration-150">
         Save
