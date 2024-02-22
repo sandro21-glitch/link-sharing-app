2233cf6d0ca71a891c15673b8ecb369dcaac01e9
Sandro <vipsandro11@gmail.com>
2024-02-22 18:29:34 +0400

    refactor: Update AddNewLink component

--- src/features/account/linksPage/customizeLinks/AddNewLink.tsx
+++ src/features/account/linksPage/customizeLinks/AddNewLink.tsx
@@ -1,9 +1,11 @@
-import { useAppDispatch } from "../../../../hooks/reduxHooks";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/reduxHooks";
 import { LINK_SITES } from "../../../../platforms";
 import { addNewLink } from "../../../links";
 
 const AddNewLink = () => {
+  const { links } = useAppSelector((store) => store.links);
   const dispatch = useAppDispatch();
+  const isLinksFull = links.length === 5;
   const firstLink = {
     name: LINK_SITES[0].name,
     path: "",
@@ -13,11 +15,20 @@ const AddNewLink = () => {
     color: LINK_SITES[0].color,
     validation: LINK_SITES[0].validation,
   };
+
   return (
     <button
       type="button"
       onClick={() => dispatch(addNewLink(firstLink))}
-      className="mb-5 border border-strongPurple w-full py-3 rounded-lg text-strongPurple font-medium hover:bg-veryLightPurple transition-colors ease-in duration-150"
+      disabled={isLinksFull}
+      className={`
+      ${
+        isLinksFull
+          ? "cursor-not-allowed bg-gray-300 text-gray-700 border-transparent hover:"
+          : "hover:bg-veryLightPurple"
+      }
+      mb-5 border border-strongPurple w-full py-3 rounded-lg
+       text-strongPurple font-medium  transition-colors ease-in duration-150`}
     >
       + Add new link
     </button>
