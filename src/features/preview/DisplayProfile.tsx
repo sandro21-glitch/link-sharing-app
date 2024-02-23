6391a0fa935e1ba9ea3f336180c0414e82e1a222
Sandro <vipsandro11@gmail.com>
2024-02-23 22:51:16 +0400

    style: Adjust styling in DisplayProfile component

--- src/features/preview/DisplayProfile.tsx
+++ src/features/preview/DisplayProfile.tsx
@@ -2,8 +2,8 @@ import UserInfo from "./userSettings/UserInfo";
 
 const DisplayProfile = () => {
   return (
-    <article className="absolute top-[208px] left-0 right-0 mx-auto
-     w-[349px] min-h-[569px] rounded-[24px] py-[48px] bg-white shadow-lg flex flex-col items-center ">
+    <article className="absolute top-[150px] lg:top-[208px] left-0 right-0 mx-auto
+     lg:w-[349px] lg:min-h-[569px] lg:rounded-[24px] py-[48px] lg:bg-white lg:shadow-lg flex flex-col items-center ">
       <UserInfo />
     </article>
   );
