4f80fb6370ec26fec05d1e43a75769321afd40cc
Sandro <vipsandro11@gmail.com>
2024-02-23 23:03:06 +0400

    style: Adjust layout in SaveBtn component

--- src/features/account/linksPage/customizeLinks/SaveBtn.tsx
+++ src/features/account/linksPage/customizeLinks/SaveBtn.tsx
@@ -14,9 +14,10 @@ const SaveBtn = () => {
       disabled={areAllLinksValid ? false : true}
       className={`${
         !areAllLinksValid ? "cursor-not-allowed" : "cursor-pointer"
-      } mt-5 w-full  border border-transparent border-t-[#d9d9d9] flex justify-end`}
+      } mt-5 w-full border border-transparent border-t-[#d9d9d9] flex justify-center lg:justify-end`}
     >
-      <div className="text-white flex justify-center items-center mt-5 bg-strongPurple font-semibold capitalize w-[91px] h-[46px] rounded-[8px] hover:opacity-60 transition-opacity ease-in duration-150">
+      <div className="text-white flex justify-center items-center mt-5 bg-strongPurple
+       font-semibold capitalize w-[90%] lg:w-[91px] h-[46px] rounded-[8px] hover:opacity-60 transition-opacity ease-in duration-150">
         Save
       </div>
     </button>
