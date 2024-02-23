272b41dc050b09245201671829be5bb04e8391e0
Sandro <vipsandro11@gmail.com>
2024-02-23 22:43:38 +0400

    update DisplayProfile.tsx

--- src/features/preview/DisplayProfile.tsx
+++ src/features/preview/DisplayProfile.tsx
@@ -2,7 +2,8 @@ import UserInfo from "./userSettings/UserInfo";
 
 const DisplayProfile = () => {
   return (
-    <article className="absolute top-[208px] left-0 right-0 mx-auto w-[349px] min-h-[569px] rounded-[24px] py-[48px] bg-white shadow-lg flex flex-col items-center ">
+    <article className="absolute top-[208px] left-0 right-0 mx-auto
+     w-[349px] min-h-[569px] rounded-[24px] py-[48px] bg-white shadow-lg flex flex-col items-center ">
       <UserInfo />
     </article>
   );
