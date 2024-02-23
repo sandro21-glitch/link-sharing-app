fcea2b7df84f54c3400abb9176c66efc9032661f
Sandro <vipsandro11@gmail.com>
2024-02-23 22:33:58 +0400

    update BasicDetails.tsx

--- src/features/account/profilePage/profileDetails/BasicDetails.tsx
+++ src/features/account/profilePage/profileDetails/BasicDetails.tsx
@@ -1,28 +1,43 @@
 const BasicDetails = () => {
   return (
     <section className="mb-5 px-5 flex flex-col justify-center gap-[12px] items-center mx-auto bg-[#fafafa] w-[85%] h-[233px]">
-      <div className="w-full flex justify-between items-center">
-        <label htmlFor="name" className="text-[1rem]">
+      <div className="w-full flex flex-col lg:flex-row items-start justify-between lg:items-center">
+        <label htmlFor="name" className="text-[.7rem]">
           First name*
         </label>
         <div className="w-[432px]">
-          <input id="name" type="text" className="w-full h-[48px] border rounded-lg outline-none focus:border-strongPurple transition-all ease-in duration-150 px-4" />
+          <input
+            id="name"
+            type="text"
+            placeholder="Text input"
+            className="w-full h-[48px] border rounded-lg outline-none focus:border-strongPurple transition-all ease-in duration-150 px-4"
+          />
         </div>
       </div>
-      <div className="w-full  flex justify-between items-center">
-        <label htmlFor="lastName" className="text-[1rem]">
-        Last name*
+      <div className="w-full flex flex-col lg:flex-row items-start justify-between lg:items-center">
+        <label htmlFor="lastName" className="text-[.7rem]">
+          Last name*
         </label>
         <div className="w-[432px]">
-          <input id="lastName" type="text" className="w-full h-[48px] border rounded-lg outline-none focus:border-strongPurple transition-all ease-in duration-150 px-4" />
+          <input
+            id="lastName"
+            type="text"
+            placeholder="Text input"
+            className="w-full h-[48px] border rounded-lg outline-none focus:border-strongPurple transition-all ease-in duration-150 px-4"
+          />
         </div>
       </div>
-      <div className="w-full  flex justify-between items-center">
-        <label htmlFor="email" className="text-[1rem]">
-        Email
+      <div className="w-full flex flex-col lg:flex-row items-start justify-between lg:items-center">
+        <label htmlFor="email" className="text-[.7rem]">
+          Email
         </label>
         <div className="w-[432px]">
-          <input id="email" type="email" className="w-full h-[48px] border rounded-lg outline-none focus:border-strongPurple transition-all ease-in duration-150 px-4" />
+          <input
+            id="email"
+            type="email"
+            placeholder="Text input"
+            className="w-full h-[48px] border rounded-lg outline-none focus:border-strongPurple transition-all ease-in duration-150 px-4"
+          />
         </div>
       </div>
     </section>
