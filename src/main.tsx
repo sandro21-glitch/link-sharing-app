a71c05c8614fa7975232a4aa2a5e944499b244aa
Sandro <vipsandro11@gmail.com>
2024-02-24 16:59:44 +0400

    refactor: Indent nested components consistently in ReactDOM.render

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { store } from "./store.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+        <App />
     </Provider>
   </React.StrictMode>
 );
