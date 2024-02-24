8390e096e07703ae73c6bab6dbc72192ae864160
Sandro <vipsandro11@gmail.com>
2024-02-24 17:02:39 +0400

    feat: Integrate drag-and-drop functionality in LinkBoxes component

--- src/features/account/linksPage/addedLinks/LinkBoxes.tsx
+++ src/features/account/linksPage/addedLinks/LinkBoxes.tsx
@@ -1,16 +1,22 @@
+import DndProviderWrapper from "../../../../hooks/DndProviderWrapper";
+import DroppableArea from "../../../../hooks/DroppableArea";
 import { useAppSelector } from "../../../../hooks/reduxHooks";
 import SingleLinkBox from "./SingleLinkBox";
 
 const LinkBoxes = () => {
   const { links } = useAppSelector((store) => store.links);
+
   return (
-    <div className="absolute top-[278px] left-0 right-0 mx-auto w-[237px] flex flex-col gap-[20px]">
-      {/* single box */}
-      {links.map((link) => {
-        const { id } = link;
-        return <SingleLinkBox key={id} link={link} />;
-      })}
-    </div>
+    <DndProviderWrapper>
+      <div className="absolute top-[278px] left-0 right-0 mx-auto w-[237px] flex flex-col gap-[20px]">
+        {/* single box */}
+        {links.map((link) => (
+          <DroppableArea key={link.id} dropAreaId={link.id}>
+            <SingleLinkBox link={link} />
+          </DroppableArea>
+        ))}
+      </div>
+    </DndProviderWrapper>
   );
 };
 
