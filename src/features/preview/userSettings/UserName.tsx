af31091bc9aa7d3e3b0bf2017af7ba2ac5d315a3
Sandro <vipsandro11@gmail.com>
2024-02-25 20:56:07 +0400

    refactor: Update UserName component to use Redux store

--- src/features/preview/userSettings/UserName.tsx
+++ src/features/preview/userSettings/UserName.tsx
@@ -1,9 +1,10 @@
-
+import { useAppSelector } from "../../../hooks/reduxHooks";
 
 const UserName = () => {
-  return (
-    <h3 className="font-semibold">Sandro Papidze</h3>
-  )
-}
+  const {
+    userData: { userName },
+  } = useAppSelector((store) => store.user);
+  return <h3 className="font-semibold">{userName ? userName : ""}</h3>;
+};
 
-export default UserName
+export default UserName;
