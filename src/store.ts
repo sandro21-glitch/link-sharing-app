98193bca6bd88ac804949c7bc5468518fb123565
Sandro <vipsandro11@gmail.com>
2024-02-25 20:54:10 +0400

    chore: update Redux store configuration

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import linksReducer from "./features/links";
+import userReducer from "./features/user";
 export const store = configureStore({
   reducer: {
     links: linksReducer,
+    user: userReducer,
   },
 });
 
