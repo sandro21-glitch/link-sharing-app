762f5f7746418c4be8e3addadc0c96fc85ee2fc9
Sandro <vipsandro11@gmail.com>
2024-02-25 20:55:34 +0400

    feat: Integrate Redux state management in ProfileDetails component

--- src/features/account/profilePage/ProfileDetails.tsx
+++ src/features/account/profilePage/ProfileDetails.tsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import BasicDetails from "./profileDetails/BasicDetails";
 import UploadImage from "./profileDetails/UploadImage";
+import { useAppDispatch } from "../../../hooks/reduxHooks";
+import { setUserDataState } from "../../user";
 
 const ProfileDetails = () => {
+  const dispatch = useAppDispatch();
+  const [userData, setUserData] = useState({
+    userName: "",
+    userLastName: "",
+    email: "",
+  });
+  const handleSubmitUserData = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(
+      setUserDataState({
+        data: {
+          userName: userData.userName,
+          lastName: userData.userLastName,
+          email: userData.email,
+        },
+      })
+    );
+  };
+
   return (
-    <form className="bg-white flex-1 rounded-[12px]">
+    <form
+      onSubmit={handleSubmitUserData}
+      className="bg-white flex-1 rounded-[12px]"
+    >
       <h1 className="text-[2rem] font-semibold normal-case pt-10 px-10 mb-5">
         Profile Details
       </h1>
@@ -11,7 +36,7 @@ const ProfileDetails = () => {
         Add your details to create a personal touch to your profile.
       </p>
       <UploadImage />
-      <BasicDetails />
+      <BasicDetails userData={userData} setUserData={setUserData} />
       <div className="mt-5 border border-transparent border-t-[#d9d9d9] flex justify-center lg:justify-end">
         <button
           type="submit"
