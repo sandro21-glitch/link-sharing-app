d6423929e5e9acadcf2241cfa32e2ea40519b500
Sandro <vipsandro11@gmail.com>
2024-02-26 20:47:55 +0400

    feat(username): display both userName and lastName in UserName component

--- src/features/preview/userSettings/UserName.tsx
+++ src/features/preview/userSettings/UserName.tsx
@@ -2,9 +2,20 @@ import { useAppSelector } from "../../../hooks/reduxHooks";
 
 const UserName = () => {
   const {
-    userData: { userName },
+    userData: { userName, lastName },
   } = useAppSelector((store) => store.user);
-  return <h3 className="font-semibold">{userName ? userName : ""}</h3>;
+  return (
+    <h3 className="font-semibold">
+      {userName && lastName ? (
+        <div className="flex items-center gap-2">
+          <span>{userName}</span>
+          <span>{lastName}</span>
+        </div>
+      ) : (
+        ""
+      )}
+    </h3>
+  );
 };
 
 export default UserName;
