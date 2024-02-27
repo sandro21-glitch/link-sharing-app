337a709b16272fc3fb0fa1377f3321407fd89c0f
Sandro <vipsandro11@gmail.com>
2024-02-27 22:32:06 +0400

    refactor: improved structure and readability

--- src/features/account/linksPage/customizeLinks/SaveBtn.tsx
+++ src/features/account/linksPage/customizeLinks/SaveBtn.tsx
@@ -8,23 +8,24 @@ const SaveBtn = () => {
     const regexPattern = new RegExp(patternWithoutSlashes);
     return regexPattern.test(link.path) || link.path !== "";
   });
-  
+
   return (
-    <button
-      type="submit"
-      onClick={() => areAllLinksValid && toast.success("Your changes have been successfully saved!")}
-      disabled={areAllLinksValid ? false : true}
-      className={`${
-        !areAllLinksValid ? "cursor-not-allowed" : "cursor-pointer"
-      } mt-5 w-full border border-transparent border-t-[#d9d9d9] flex justify-center lg:justify-end`}
-    >
-      <div
-        className="text-white flex justify-center items-center mt-5 bg-strongPurple
-       font-semibold capitalize w-[90%] lg:w-[91px] h-[46px] rounded-[8px] hover:opacity-60 transition-opacity ease-in duration-150"
+    <div className="mt-5 w-full border border-transparent border-t-[#d9d9d9] flex justify-center lg:justify-end">
+      <button
+        type="submit"
+        onClick={() =>
+          areAllLinksValid &&
+          toast.success("Your changes have been successfully saved!")
+        }
+        disabled={areAllLinksValid ? false : true}
+        className={`${
+          !areAllLinksValid ? "cursor-not-allowed" : "cursor-pointer"
+        } text-white flex justify-center items-center mt-5 bg-strongPurple
+        font-semibold capitalize w-[90%] lg:w-[91px] h-[46px] rounded-[8px] hover:opacity-60 transition-opacity ease-in duration-150`}
       >
         Save
-      </div>
-    </button>
+      </button>
+    </div>
   );
 };
 
