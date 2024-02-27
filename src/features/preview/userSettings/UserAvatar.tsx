30e43ab9195490a8e8b329be2a074ca3cfa9ce9a
Sandro <vipsandro11@gmail.com>
2024-02-27 22:05:41 +0400

    feat: Integrate user image from Redux in UserAvatar component

--- src/features/preview/userSettings/UserAvatar.tsx
+++ src/features/preview/userSettings/UserAvatar.tsx
@@ -1,6 +1,11 @@
+import { useAppSelector } from "../../../hooks/reduxHooks";
+
 const UserAvatar = () => {
+  const { image } = useAppSelector((store) => store.user);
   return (
-    <div className="rounded-full border-4 border-strongPurple w-[6rem] h-[6rem] mb-7"></div>
+    <div className="rounded-full overflow-hidden border-4 border-strongPurple w-[6rem] h-[6rem] mb-7">
+      {image !== "" && <img src={image} alt="user image" />}
+    </div>
   );
 };
 
