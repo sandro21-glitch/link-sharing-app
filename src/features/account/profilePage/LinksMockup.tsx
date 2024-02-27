5ff2c1e1620eae88ac588c258d96a682c103cf52
Sandro <vipsandro11@gmail.com>
2024-02-27 21:51:31 +0400

    feat(LinksMockup): conditionally display user image

--- src/features/account/profilePage/LinksMockup.tsx
+++ src/features/account/profilePage/LinksMockup.tsx
@@ -1,14 +1,21 @@
+import { useAppSelector } from "../../../hooks/reduxHooks";
 import LinkBoxes from "../linksPage/addedLinks/LinkBoxes";
 import phoneMockup from "/assets/images/illustration-phone-mockup.svg";
 const LinksMockup = () => {
+  const { image } = useAppSelector((store) => store.user);
   return (
     <section className="bg-white rounded-[12px] w-[512px] h-[834px] hidden lg:flex-center ">
       <div className="relative">
         <img
           src={phoneMockup}
           alt="phoneMockup"
-          className="min-w-[307px] h-auto object-contain"
+          className="min-w-[307px] h-auto object-contain "
         />
+        {image && (
+          <div className="absolute mx-auto top-[63.5px] w-[96px] h-[96px] overflow-hidden left-0 right-0 rounded-full border-4 border-strongPurple">
+            <img src={image} alt="user image" />
+          </div>
+        )}
         <div className="absolute bottom-[57px] w-[237px] h-[300px] bg-white left-0 right-0 mx-auto"></div>
         <LinkBoxes />
       </div>
